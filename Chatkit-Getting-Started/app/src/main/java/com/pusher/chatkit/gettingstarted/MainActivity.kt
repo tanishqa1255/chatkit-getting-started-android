65a504e96b7a73a298d126a399f0089323cb0cd5
Danielle Vass <danielle.vass@pusher.com>
2019-08-14 17:12:58 +0100

    Remove chatkit specific code

--- Chatkit-Getting-Started/app/src/main/java/com/pusher/chatkit/gettingstarted/MainActivity.kt
+++ Chatkit-Getting-Started/app/src/main/java/com/pusher/chatkit/gettingstarted/MainActivity.kt
@@ -2,14 +2,10 @@ package com.pusher.chatkit.gettingstarted
 
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.pusher.chatkit.*
+import com.pusher.chatkit.CurrentUser
 import com.pusher.chatkit.rooms.Room
-import com.pusher.chatkit.rooms.RoomListeners
-import com.pusher.util.Result
-import elements.Error
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,69 +31,15 @@ class MainActivity : AppCompatActivity() {
         recyclerViewMessages.layoutManager =  layoutManager
         recyclerViewMessages.adapter = adapter
 
-        //create the chat manager
-        val chatManager = ChatManager(
-            instanceLocator = INSTANCE_LOCATOR,
-            userId = userId,
-            dependencies = AndroidChatkitDependencies(
-                tokenProvider = ChatkitTokenProvider(
-                    endpoint = TOKEN_PROVIDER_URL,
-                    userId = userId
-                ),
-                context = this
-            )
-        )
+        //TODO: your Chatkit code here :-)
 
-        //attempt to connect
-        chatManager.connect(
-            listeners = ChatListeners(),
-            callback = { result ->
-                when (result) {
-                    is Result.Success -> {
-                        result.value.let { user ->
-
-                            //success! save the user and the room to be able to use later
-                            currentUser = user
-                            currentRoom = user.rooms.first()
-
-                            runOnUiThread {
-                                //display what room we're in on the top of the screen
-                                lblTopBarTitle.text = currentRoom.name
-                            }
-
-                            //subscribe to room
-                            user.subscribeToRoomMultipart(
-                                currentRoom.id,
-                                RoomListeners(onMultipartMessage = { message ->
-                                    runOnUiThread {
-                                        //add the message to our adapter and scroll to the new message
-                                        adapter.addMessage(message)
-                                        recyclerViewMessages.layoutManager?.scrollToPosition(adapter.messages.size -1)
-                                    }
-                                }),
-                                messageLimit = 20,
-                                callback = {})
-
-                        }
-                    }
-
-                    is Error -> {
-                        Toast.makeText(this, result.reason, Toast.LENGTH_SHORT).show() }
-                }
-            }
-        )
 
         txtMessage.setOnEditorActionListener { v, actionId, event ->
 
             //if the action button was send (replaces the enter button)
             if(actionId == EditorInfo.IME_ACTION_SEND){
 
-                //send the message using chatkit
-                currentUser.sendSimpleMessage(
-                    roomId = currentRoom.id,
-                    messageText = txtMessage.text.toString(),
-                    callback = {  }
-                )
+                //TODO: send message using chatkit
 
                 //clear the edit text
                 txtMessage.setText("")
