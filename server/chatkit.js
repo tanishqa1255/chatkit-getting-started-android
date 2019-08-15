3cbf1a9239b8224759d47d2eae6097b8e68c6f34
Danielle Vass <danielle.vass@pusher.com>
2019-08-15 15:02:06 +0100

    remove credentials

--- server/chatkit.js
+++ server/chatkit.js
@@ -1,9 +1,9 @@
 const Chatkit = require("@pusher/chatkit-server")
 
 const chatkit = new Chatkit.default({
-  instanceLocator: "v1:us1:2641cd1b-0229-4958-a7d6-d1d3527fab55",
+  instanceLocator: "YOUR_INSTANCE_LOCATOR",
   key:
-    "46e610c9-9f6d-44c9-9852-462571289772:Y71AQamGMnf6K2KuM+bKilUtMqaYOrL/omrBPcexOlQ="
+    "YOUR_SECRET_KEY"
 })
 
 module.exports = chatkit
