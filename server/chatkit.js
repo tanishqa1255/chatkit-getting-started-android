0b5316ea1e7b85078ebc47a9599df97a653d73b1
tanishqa1255 <48148167+tanishqa1255@users.noreply.github.com>
2020-01-07 21:06:18 +0530

    Update chatkit.js

--- server/chatkit.js
+++ server/chatkit.js
@@ -1,9 +1,9 @@
 const Chatkit = require("@pusher/chatkit-server")
 
 const chatkit = new Chatkit.default({
-  instanceLocator: "YOUR_INSTANCE_LOCATOR",
+  instanceLocator: "v1:us1:98bbf1de-0845-41c1-a30b-7faf6ba97898",
   key:
-    "YOUR_SECRET_KEY"
+    "ca8fd84b-cb98-4325-90d4-e8702ab687bd:v0Grio0HSsxUM60PiYjvJkkWeqeH4rVRYwyvTmjZr+E="
 })
 
 module.exports = chatkit
